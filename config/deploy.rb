f3da6a1bd09b850fea44d10ead7345996de5548b
Jacob Smergel <jacob.smergel@gmail.com>
2015-11-24 20:59:54 -0500

    vps

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "albumExchange"
-set :repo_url, "git@github.com/Smergel.albumExchange.git"
+set :repo_url, "https://github.com/Smergel/albumExchange"
 set :linked_dirs, %w(
   bin log vendor/bundle public/system
   tmp/pids tmp/cache tmp/sockets
